7108a6732412ae8c9968cff15de65d6e421904f5
Mark Underseth <marku@s2technologies.com>
2017-04-08 14:20:02 +0000

    c9 2 options

--- c9build.sh
+++ c9build.sh
@@ -2,10 +2,16 @@
 
 # Download Client
 curl -s https://testspace-client.s3.amazonaws.com/testspace-linux-dev.tgz | sudo tar -zxvf- -C /usr/local/bin
+
 # using "mark-s2-tec"
 export MARK_TOKEN=095d8acfaaf71b4a0f3767e41152620ebdda1b31
-BRANCH_NAME=`git symbolic-ref --short HEAD` 
-testspace config url http://$MARK_TOKEN@mark.stridespace.com/munderseth:box_CIs_matrix/$BRANCH_NAME
+
+# Option 1
+#BRANCH_NAME=`git symbolic-ref --short HEAD` 
+#testspace config url http://$MARK_TOKEN@mark.stridespace.com/munderseth:box_CIs_matrix/$BRANCH_NAME
+
+# Option 2 
+CI=true testspace config url http://$MARK_TOKEN@mark.stridespace.com
 
 # Build
 
