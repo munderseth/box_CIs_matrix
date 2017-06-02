d6397fdf33b3c750e80d15b47a74ba34a70bcc67
Mark Underseth <marku@s2technologies.com>
2017-06-02 19:19:27 +0000

    clean up based on help

---
+++ README.md
@@ -0,0 +1,8 @@
+The Repo is used for simple Matrix examples based on [this help](https://help.testspace.com/how-to:add-to-ci) article:
+
+ * Traivis CI
+ * Shippable
+ * Appveyor
+ 
+All CIs push to the same Space / Results set. 
+ 
--- c9build.sh
+++ c9build.sh
@@ -3,7 +3,7 @@
 # Client install and configure (using "mark-s2-tec")
 mkdir -p $HOME/bin
 curl -s https://testspace-client.s3.amazonaws.com/testspace-linux-dev.tgz | tar -zxvf- -C $HOME/bin
-CI=true testspace config url http://095d8acfaaf71b4a0f3767e41152620ebdda1b31:@mark.stridespace.com
+CI=true testspace config url http://mark.stridespace.com
 testspace -v
 
 # Test
